46bdff3bb215e85f47684b3b40dff9f31b8de14f
Duy Trung <hqkv1hcm@gmail.com>
2026-07-18 20:13:47 +0700

    Fix duong dan ptpl

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   site: 'https://thongquantaptrung.com',
   base: '/ptpl', // Đường dẫn thư mục con thực tế
-  outDir: '../dist/ptpl', // <--- THÊM DÒNG NÀY VÀO ĐÂY
+  outDir: './dist/ptpl', // <--- THÊM DÒNG NÀY VÀO ĐÂY
   server: {
     // Ép buộc máy chủ thử nghiệm cục bộ cũng phải chạy dưới đường dẫn /ptpl
     headers: {
