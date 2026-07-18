b7ea3f498ba93d4d2058e549f8827d65d661f454
Duy Trung <hqkv1hcm@gmail.com>
2026-07-18 19:16:20 +0700

    feat: cap nhat cấu hình astro va giao dien ptpl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://thongquantaptrung.com',
+  base: '/ptpl', // Đường dẫn thư mục con thực tế
+  outDir: '../dist/ptpl', // <--- THÊM DÒNG NÀY VÀO ĐÂY
+  server: {
+    // Ép buộc máy chủ thử nghiệm cục bộ cũng phải chạy dưới đường dẫn /ptpl
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    }
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
